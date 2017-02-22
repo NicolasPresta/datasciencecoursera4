6661c0ba90e1dad9e736ba95b68eed4f32766801
nicolaspresta <presta_26@hotmail.com>
2017-02-22 02:22:46 -0300

    listo

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Course 4 Data Science - Exploratory Data Analysis - Course Project 1
+
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# 1º Graph
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+              
